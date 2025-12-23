611bc1b8464dd1d295c0a40375be02ba39e946c9
JOOHYUN PARK <zzoohyun99@kakao.com>
2025-12-24 08:47:10 +0900

    20251224-initial-upload

---
+++ src/main/java/com/shopping/mall/home/HomeController.java
@@ -0,0 +1,14 @@
+package com.shopping.mall.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    
+    @GetMapping("/")
+    public String main() {
+        return "index";
+    }
+    
+}
